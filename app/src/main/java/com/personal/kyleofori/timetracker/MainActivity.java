8890a92613e2a69e36994c8368f8883a355969d5
Kyle Ofori <kd.p.ofori@gmail.com>
2016-07-25 10:34:35 -0400

    Include pre-populated list view

--- app/src/main/java/com/personal/kyleofori/timetracker/MainActivity.java
+++ app/src/main/java/com/personal/kyleofori/timetracker/MainActivity.java
@@ -1,13 +1,23 @@
 package com.personal.kyleofori.timetracker;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 }
