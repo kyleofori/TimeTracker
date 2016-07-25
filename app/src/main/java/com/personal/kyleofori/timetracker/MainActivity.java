2b4ec857d77630b651ec66cb105b37dc38d9f02c
Kyle Ofori <kd.p.ofori@gmail.com>
2016-07-25 10:38:59 -0400

    Create placeholder toast in onListItemClick()

--- app/src/main/java/com/personal/kyleofori/timetracker/MainActivity.java
+++ app/src/main/java/com/personal/kyleofori/timetracker/MainActivity.java
@@ -3,7 +3,10 @@ package com.personal.kyleofori.timetracker;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
@@ -19,5 +22,12 @@ public class MainActivity extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_SHORT).show();
     }
 }
